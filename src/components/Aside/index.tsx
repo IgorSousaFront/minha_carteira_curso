4468cbea647ea190a729ccbb5e6fd01995effbe1
Igor Sousa <90005194@pmenos.com.br>
2021-11-30 18:59:14 -0300

    finalizar barra lateral de navegação

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 
-import { Container } from './styles';
+import LogoAsset from '../../assets/logo.svg'
+
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp } from 'react-icons/md'
+
+import {
+  Container,
+  Header,
+  LogoImg,
+  MenuContainer,
+  MenuItemLink,
+  Title
+} from './styles';
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogoImg src={LogoAsset} alt="Logo Minha Carteira" />
+        <Title>Minha carteira</Title>
+      </Header>
+      
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Entradas
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Saídas
+        </MenuItemLink>
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -2,6 +2,45 @@ import styled from 'styled-components'
 
 export const Container = styled.aside`
   grid-area: AS;
-  color: ${props => props.theme.colors.white};
+  padding-left: 20px;
   background-color: ${props => props.theme.colors.secondary};
+  border-right: 1px solid ${props => props.theme.colors.grey};
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  height: 70px;
+`;
+
+export const LogoImg = styled.img`
+  height: 40px;
+  width: 40px;
+  margin-right: 20px;
+`;
+
+export const Title = styled.h3`
+  color: ${props => props.theme.colors.white};
+`;
+
+export const MenuContainer = styled.nav`
+  margin-top: 20px;
+`;
+
+export const MenuItemLink = styled.a`
+  color: ${props => props.theme.colors.info};
+  text-decoration: none;
+  transition: opacity .3s;
+  display: flex;
+  align-items: center;
+  margin: 0 0 16px;
+
+  > svg {
+    font-size: 20px;
+    margin-right: 5px;
+  }
+
+  &:hover {
+    opacity: .7;
+  }
 `;
