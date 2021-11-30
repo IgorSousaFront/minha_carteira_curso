852a37c99be42e504212e3f87e180bfb446ad5be
Igor Sousa <90005194@pmenos.com.br>
2021-11-30 10:22:21 -0300

    setando css global

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './styles/GlobalStyles';
+import Dashboard from './pages/Dashboard';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Eita massa demais</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda React + Typescript
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <Dashboard />
+    </>
   );
 }
 
